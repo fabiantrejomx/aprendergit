4823b5dd6d1320c3f1a3aac66aaab5a320770903
fabianTrejo <winer453@gmail.com>
2016-03-20 19:16:12 -0600

    acabe con jquery para un pequeño efecto

--- script.js
+++ script.js
@@ -9,3 +9,18 @@ $('.a').on('click', function(event) {
 
 });
 */
+$(document).on("ready",function(){
+
+$(".elementos").on("click",".label",function(){
+	var este= $(this);
+	var cont= este.next();
+
+	//recorro a todo los demas
+	var allcont=este.parent().siblings().find('.contenido');
+	cont.slideToggle();
+	allcont.slideUp();
+	
+});
+
+
+});
